1087ce7f8f29339043b8e30205d251c18350ebbe
Nicholas Birochi <131502494+nicholasbirochi@users.noreply.github.com>
2024-12-03 14:47:42 -0300

    Update Respostas.R
    Exercício Concluído!

--- 4 - Matrizes/Soluções/Respostas.R
+++ 4 - Matrizes/Soluções/Respostas.R
@@ -1,8 +1,19 @@
-NomeCliente <- c("Marcos de Aguiar", "Paula Miranda", "Emilio Santos", "Pryscilla de Albuquerque")
-Salario <- c(4000, 2500, 1200, 3400)
-Idade <- c(44, 56, 76, 40)
+#1.Fazendo a Tabela:
+nomeCliente <- c("Marcos de Aguiar", "Paula Miranda", "Emilio Santos", "Pryscilla de Albuquerque")
+salario <- c("4000", "2500", "1200", "3400")
+idade <- c(44, 56, 76, 40)
 
 
-TabelaGeral <-cbind(NomeCliente, Salario, Idade)
+Emprestimos <-cbind(nomeCliente, salario, idade)
 
-TabelaGeral[2,2] <- c(5500.33)
+#2.Modificando um salário:
+Emprestimos[2,2] <- c("5500.33")
+
+#3.Salário nruto para líquido:
+Emprestimos[,2] <-as.numeric(Emprestimos[,2])-as.numeric(Emprestimos[,2])*0.3
+
+#4.Média salarial:
+mean(as.numeric(Emprestimos[,2]))
+
+#5.Modificar todos os nomes:
+colnames(Emprestimos) <- c("NomeCliente", "SalarioLiquido", "Idade")
